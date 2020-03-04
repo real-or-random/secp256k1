8690fe3626b815028732a715d3c34245ce2a45fa
Tim Ruffing <crypto@timruffing.de>
2020-03-04 17:33:28 +0100

    Add valgrind constant-time test to `make check`

---
+++ valgrind_ctime_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+libtool --mode=execute valgrind --error-exitcode=1 ./valgrind_ctime_test "$@"
+
+case $? in
+    127) # "command not found", i.e., either libtool or valgrind not installed
+        exit 77 # map this to "SKIP" (=77) for make check
+        ;;
+    *)
+        exit $?
+        ;;
+esac
