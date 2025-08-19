516ffff0a17aea92a25ca1fc60e494e1a9e82270
Tim Ruffing <me@real-or-random.org>
2025-08-19 09:16:37 +0200

    fixup! silentpayments: sending
    I don't think this comment is necessary. We always try to clear as early
    as possible, so there's not much to explain here. (But I wouldn't mind
    keeping the comment.)

--- src/modules/silentpayments/main_impl.h
+++ src/modules/silentpayments/main_impl.h
@@ -248,9 +248,6 @@ int secp256k1_silentpayments_sender_create_outputs(
     /* If there are any failures in loading/summing up the secret keys, fail early */
     sum_is_zero = secp256k1_scalar_is_zero(&a_sum_scalar);
     secp256k1_declassify(ctx, &sum_is_zero, sizeof(sum_is_zero));
-    /* Clear the addend variable as this is no longer needed at this point
-     * and contains secret data. This saves from needing to remember to clear
-     * this variable from multiple places below */
     secp256k1_scalar_clear(&addend);
     if (sum_is_zero) {
         secp256k1_scalar_clear(&a_sum_scalar);
