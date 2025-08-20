e454cb349919b6562fa025ffe86c9ef79dc29182
Tim Ruffing <me@real-or-random.org>
2025-08-20 13:55:47 +0200

    ...

--- examples/recovery.c
+++ examples/recovery.c
@@ -1,15 +1,12 @@
 #include <string.h>
-#include <stdio.h>
 
 int main(void) {
     unsigned char buf1[33];
     unsigned char buf2[33];
-    int ret;
 
     memset(buf1, 5, 33);
     memset(buf2, 5, 33);
-    ret = memcmp(buf1, buf2, 33);
-    printf("%d\n", ret);
+    memcmp(buf1, buf2, 33);
 
     return 0;
 }
