74b077ac1ba8eb2e0e828b5a951ed9705402da42
Tim Ruffing <me@real-or-random.org>
2025-08-20 13:52:56 +0200

    ....

--- examples/recovery.c
+++ examples/recovery.c
@@ -5,10 +5,9 @@ int main(void) {
     unsigned char buf1[33];
     unsigned char buf2[33];
     int ret;
-    void *(*volatile const volatile_memset)(void *, int, size_t) = memset;
 
     memset(buf1, 5, 33);
-    volatile_memset(buf2, 5, 33);
+    memset(buf2, 5, 33);
     ret = memcmp(buf1, buf2, 33);
     printf("%d\n", ret);
 
