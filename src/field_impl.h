18ffa54493288ce5868054b366784944248581fb
Tim Ruffing <me@real-or-random.org>
2026-08-14 21:35:53 +0200

    HACK Verify that magnitude never exceeds 8

--- src/field_impl.h
+++ src/field_impl.h
@@ -151,8 +151,8 @@ static void secp256k1_fe_verify_magnitude(const secp256k1_fe *a, int m) { (void)
 #else
 static void secp256k1_fe_impl_verify(const secp256k1_fe *a);
 static void secp256k1_fe_verify(const secp256k1_fe *a) {
-    /* Magnitude between 0 and 31. */
-    SECP256K1_FE_VERIFY_MAGNITUDE(a, 31);
+    /* Magnitude must be between 0 and 31; actually it's even between 0 and 8. */
+    SECP256K1_FE_VERIFY_MAGNITUDE(a, 8);
     /* Normalized is 0 or 1. */
     VERIFY_CHECK((a->normalized == 0) || (a->normalized == 1));
     /* If normalized, magnitude must be 0 or 1. */
